a5cd695e8475776688b07abef14d6a520dca0cb5
Davide <davide.mattioli.2001@gmail.com>
2025-11-26 18:40:59 +0100

    feat: update raw and processed directory paths for improved data management

--- experiments/dinov3Seg/main.py
+++ experiments/dinov3Seg/main.py
@@ -45,9 +45,9 @@ os.environ.setdefault("MKL_NUM_THREADS", "1")
 os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
 os.environ.setdefault("PYTORCH_CUDA_ALLOC_CONF", "expandable_segments:True")
 
-DEFAULT_RAW_IMAGES_DIR = "/home/mak/PycharmProjects/SegEdge/experiments/get_data_from_api/patches_mt/"
+DEFAULT_RAW_IMAGES_DIR = "//mnt/ceph-hdd/projects/mthesis_davide_mattioli/patches_mt/folder_1/"
 DEFAULT_LABEL_PATH = "/run/media/mak/Partition of 1TB disk/SH_dataset/planet_labels_2022.tif"
-DEFAULT_PROCESSED_DIR = "/mnt/ceph-hdd/projects/mthesis_davide_mattioli/patches_mt/folder_1/"
+DEFAULT_PROCESSED_DIR = "/mnt/ceph-hdd/projects/mthesis_davide_mattioli/processed/folder_1/"
 DEFAULT_MODEL_NAME = "facebook/dinov3-vitl16-pretrain-sat493m"
 DEFAULT_LAYERS = [5, 11, 17, 23]
 DEFAULT_HEAD = "unet"
